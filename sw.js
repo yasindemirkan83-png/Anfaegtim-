46c06785b2318187d049adda4333ed07857b4cf4
yasindemirkan83-png <yasindemirkan83@gmail.com>
2026-03-12 11:56:54 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,20 @@
 const cacheName = 'anfa-v42-cache';
-const filesToCache = ['./', './index.html', './manifest.json', './29158.png'];
+const filesToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './29158.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)));
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
 });
 
